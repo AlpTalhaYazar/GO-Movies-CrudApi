c44da8da431df347b2b46c78e0e952064c864362
Talha Yazar <talhayazarg@gmail.com>
2022-09-15 20:47:23 +0300

    Feed data to our global model

--- main.go
+++ main.go
@@ -24,6 +24,33 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 
+	movies = append(movies,
+		Movie{
+			ID:    "1",
+			Isbn:  "54215",
+			Title: "Psycho",
+			Director: &Director{
+				FirstName: "Alfred",
+				LastName:  "Hitchcock"}})
+
+	movies = append(movies,
+		Movie{
+			ID:    "2",
+			Isbn:  "62622",
+			Title: "2001: A Space Odyssey",
+			Director: &Director{
+				FirstName: "Stanley",
+				LastName:  "Kubrick"}})
+
+	movies = append(movies,
+		Movie{
+			ID:    "3",
+			Isbn:  "816692",
+			Title: "Interstellar",
+			Director: &Director{
+				FirstName: "Christopher",
+				LastName:  "Nolan"}})
+
 	fmt.Printf("Starting server at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
