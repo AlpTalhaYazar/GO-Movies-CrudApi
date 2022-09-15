08ada8659ef074684123a877763fdf8f9ff94ab6
Talha Yazar <talhayazarg@gmail.com>
2022-09-15 20:46:10 +0300

    Created Structs and defined a global variable to use for our endpoints

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 	"log"
 	"net/http"
 )
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
 func main() {
 	r := mux.NewRouter()
 
